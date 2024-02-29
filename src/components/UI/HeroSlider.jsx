e20d892bb141e560ddc889a0f22c2fae7afb6b4b
oluchid <duwend46@gmail.com>
2024-02-29 06:10:09 +0100

    removed  the login and regiseter

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -36,8 +36,8 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-02 mt0">
         <Container>`
           <div className="slider__content ">
-            <h4 className="text-light mb-3">Stay up to date with the latest trends</h4>
-            <h1 className="text-light mb-4">Experience your favorite cars in person?!</h1>
+            <h4 className="text-light mb-3">Stay up to date </h4>
+            <h1 className="text-light mb-4">Experience your favorite</h1>
 
             <button className="btn reserve__btn mt-4">
               <Link to="/cars">Buy now</Link>
